f7899f7c8abb14e0d1abd47fa8bf14553029663c
Sam Smith <sasidhar.katikela@gmail.com>
2021-12-07 09:51:50 +0530

    added env

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 8000
 
+
+const dotenv=require("dotenv");
+dotenv.config()
+const PORT = process.env.PORT||8000
 const routeHandler = require('./routes')
 const router = routeHandler()
 
 app.use(router)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
